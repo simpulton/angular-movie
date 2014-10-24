d5873057e3d0bacddee6d161434395393334f33a
Jonathan Garvey <jon.d.garvey@gmail.com>
2014-10-24 15:53:49 -0700

    Fix gallery error

--- src/app/gallery/gallery.js
+++ src/app/gallery/gallery.js
@@ -21,12 +21,20 @@ angular.module('Movie.gallery', [
 
 		enter: function(element, done) {
 			// inbound picture animation
-			TweenMax.fromTo(element, 1, {opacity: 0}, { opacity: 1, onComplete: done});
+			var x = element.hasClass('forward') ? $window.innerWidth : -$window.innerWidth;
+
+			TweenMax.fromTo(element, 1, {x:x}, { x:0, onComplete: done});
+			element.css('position', 'static');
 		},
 
 		leave: function(element, done) {
 			// outbound picture animation
-			TweenMax.fromTo(element, time, {opacity: 1}, {opacity: 0, onComplete: done});
+			var x = element.hasClass('forward') ? -$window.innerWidth : $window.innerWidth,
+					from = element.hasClass('forward') ? 0 : -element.width();
+					time = element.hasClass('forward') ? 1 : 1.7,
+
+			element.css('position', 'absolute');
+			TweenMax.fromTo(element, time, {x:from}, {x: x, onComplete: done});
 		}
 	};
 });
