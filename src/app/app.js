48d6c654cd2362a7a47f9952e0bb3caa50708c56
Jonathan Garvey <jon.d.garvey@gmail.com>
2014-12-20 23:27:33 -0600

    Organize page transition animations

--- src/app/app.js
+++ src/app/app.js
@@ -2,7 +2,6 @@ var currentSection = "";
 var lastSection = "";
 	
 angular.module('Movie', [
-	'ngAnimate',
 	'ui.router',
 	'Movie.home',
 	'Movie.gallery',
@@ -12,7 +11,8 @@ angular.module('Movie', [
 	'Movie.directives.nav',
 	'Movie.directives.thumbnail',
 	'Movie.services.preload',
-	'Movie.services.movie'
+	'Movie.services.movie',
+  'Movie.animations'
 ])
 .constant('ENDPOINT_URI', 'app/data/movie.json')
 .config(function($stateProvider, $urlRouterProvider, $sceDelegateProvider) {
@@ -33,100 +33,21 @@ angular.module('Movie', [
 	]);
 
 })
-.animation('.main-content', function($rootScope) {
-  return {
-		enter: function(element, done) {
-			var finished = function() {
-				$rootScope.$broadcast('animation-done');
-				done();
-			};
-			
-			var elementWidth = element.width();
-			var elementHeight = element.height();
-			
-			// HACK TO FIGURE OUT WHAT SECTION IS ENTERING
-			var targetSection = element.find('div').eq(0).attr('id');
-			
-			lastSection = currentSection;
-			currentSection = targetSection;
-			
-			if (targetSection == "home") {
-				switch(lastSection) {
-					case "synopsis":
-					case "cast":
-						TweenMax.fromTo( element, 1, { x: 0, y: elementHeight, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: finished});
-						break;
-					case "gallery":
-						TweenMax.fromTo( element, 1, { x: elementWidth, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: finished});
-						break;
-					case "trailer":
-						TweenMax.fromTo( element, 1, { x: -elementWidth, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: finished});
-						break;
-					case "":
-					default:						 
-						TweenMax.fromTo( element, 1, { x: -elementWidth, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: finished});
-						break;
-				}
-			} else {
-				switch(targetSection) {
-					case "synopsis":
-					case "cast":
-						TweenMax.fromTo( $('.movie-details'), 1, { x: -500, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, delay: .5 });
-						TweenMax.fromTo( element, 1, { x: 0, y: -elementHeight, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: finished});
-						break;
-					case "gallery":
-						TweenMax.fromTo( $('.movie-details'), 1, { x: -500, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, delay: .5 });
-						TweenMax.fromTo( element, 1, { x: -elementWidth, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: finished});
-						break;
-					case "trailer":
-						TweenMax.fromTo( element, 1, { x: elementWidth, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: finished});
-						break;
-				}
-			}
+.run(function($rootScope, $timeout) {
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+      // State handling for animations
 
-		},
-		leave: function(element, done) {
-			// animation for outbound page
-			
-			var elementWidth = element.width();
-			var elementHeight = element.height();
-			
-			// HACK TO FIGURE OUT WHAT SECTION YOU ARE LEAVING
-			if (lastSection != "home") {
-				switch(lastSection) {
-					case "synopsis":
-					case "cast":
-						TweenMax.to( element, 1, { x: 0, y: -elementHeight, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
-						break;
-					case "gallery":
-						TweenMax.to( element, 1, { x: -elementWidth, y: 0, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
-						break;
-					case "trailer":
-						TweenMax.to( element, 1, { x: elementWidth, y: 0, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
-						break;
-					case "":
-					default:
-						TweenMax.to( element, 1, { autoAlpha: 0, ease: Expo.easeInOut, onComplete: done });
-						break;
-				}
-				
-			} else {
-				switch(currentSection) {
-					case "synopsis":
-					case "cast":
-						TweenMax.to( element, 1, { x: 0, y: elementHeight, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
-						break;
-					case "gallery":
-						TweenMax.to( element, 1, { x: elementWidth, y: 0, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
-						break;
-					case "trailer":
-						TweenMax.to( element, 1, { x: -elementWidth, y: 0, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
-						break;
-				}
-				
-			}
-			
-		}
-	};
+      // Set Default from state to "home"
+      fromState = (fromState.name) ? fromState : {name: "home"};
 
+      var tState = toState.name.split('.'),
+          fState = fromState.name.split('.'),
+          lastIndex = tState.length - 1,
+          fLastIndex = fState.length - 1;
+
+      var tostate = tState[lastIndex];
+      var fromstate = fState[fLastIndex];
+
+      $rootScope.animation = "from-" + fromstate + "-to-" + tostate;
+    });
 });
---
+++ src/app/common/animations/movie.animations.js
@@ -0,0 +1,129 @@
+angular.module('Movie.animations', ['ngAnimate'])
+.factory('AnimationsService', function($timeout) {
+    // Note: The "leave" attribute associated with each page transition actually contains the "leave" animation
+    //       for the page transition before. This is because when state is changed, the name
+    //       of the page transition changes before the "leave" event is executed. Take, for example,
+    //       "from-home-to-home". When I refresh the page, the "enter" attribute is
+    //       selected and that animation is run. If I go to the synopsis state, the
+    //       current page transition is changed from "from-home-to-home" to "from-home-to-synopsis";
+    //       THEN the "leave" attribute is selected and the associated animation is run. This
+    //       seems clunky, and perhaps in most cases it would be; however, in this case it actually
+    //       saves us from having to separate out the logic for the home page "leave" animation.
+    var animations = {
+      // All page transition animations go here
+
+      "from-home-to-home": {
+        enter: function(element, done, props) {
+          TweenMax.fromTo( element, 1, { x:  -props.elementWidth, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: done});
+        },
+        leave: function(element, done, props) {
+          TweenMax.fromTo( element, 1, { x: 0, y: 0, autoAlpha: 1 }, { x: 0, y: props.elementHeight, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
+        }
+      },
+      "from-home-to-synopsis": {
+        enter: function(element, done, props) {
+          $timeout(function() {
+            TweenMax.fromTo( $('.movie-details'), 1, { x: -500, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, delay: .5, onComplete:done });
+          }, 500);
+          TweenMax.fromTo( element, 1, { x: 0, y: -props.elementHeight, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: done});
+        },
+        leave: function(element, done, props) {
+          TweenMax.to( element, 1, { x: 0, y: props.elementHeight, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
+        }
+      },
+      "from-synopsis-to-home": {
+        enter: function(element, done, props) {
+          TweenMax.fromTo( element, 1, { x: 0, y: props.elementHeight, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: done});
+        },
+        leave: function(element, done, props) {
+          TweenMax.to( element, 1, { x: 0, y: -props.elementHeight, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
+        }
+      },
+      "from-home-to-gallery": {
+        enter: function(element, done, props) {
+          $timeout(function() {
+            TweenMax.fromTo( $('.movie-details'), 1, { x: -500, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, delay: .5 });
+          }, 500);
+          TweenMax.fromTo( element, 1, { x: -props.elementWidth, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: done});
+        },
+        leave: function(element, done, props) {
+          TweenMax.to( element, 1, { x: props.elementWidth, y: 0, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
+        }
+      },
+      "from-gallery-to-home": {
+        enter: function (element, done, props) {
+          TweenMax.fromTo( element, 1, { x: props.elementWidth, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: done});
+        },
+        leave: function (element, done, props) {
+          TweenMax.to( element, 1, { x: -props.elementWidth, y: 0, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
+        }
+      },
+      "from-home-to-trailer": {
+        enter: function(element, done, props) {
+          TweenMax.fromTo( element, 1, { x: props.elementWidth, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: done});
+        },
+        leave: function(element, done, props) {
+          TweenMax.to( element, 1, { x: -props.elementWidth, y: 0, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
+        }
+      },
+      "from-trailer-to-home": {
+        enter: function (element, done, props) {
+          TweenMax.fromTo( element, 1, { x: -props.elementWidth, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: done});
+        },
+        leave: function (element, done, props) {
+          TweenMax.to( element, 1, { x: props.elementWidth, y: 0, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
+        }
+      }
+    };
+
+    // Synopsis and cast animations are the same
+    animations["from-home-to-cast"] = animations["from-home-to-synopsis"];
+    animations["from-cast-to-home"] = animations["from-synopsis-to-home"];
+
+    // Return page transition animations
+    var getAnimations = function() {
+      return animations;
+    };
+
+    // Return width and height of an element
+    var getProps = function(element) {
+      return {
+        elementWidth: element.width(),
+        elementHeight: element.height()
+      };
+    };
+
+    return {
+      getAnimations: getAnimations,
+      getProps: getProps
+    };
+})
+.animation('.main-content', function(AnimationsService, $rootScope) {
+  var animations = AnimationsService.getAnimations();
+
+  return {
+    enter: function(element, done) {
+      var finished = function() {
+        $rootScope.$broadcast('animation-done');
+        done();
+      };
+
+      var props = AnimationsService.getProps(element);
+
+      animations[$rootScope.animation].enter(element, finished, props);
+
+    },
+    leave: function(element, done) {
+      var props = AnimationsService.getProps(element);
+
+      animations[$rootScope.animation].leave(element, done, props);
+    }
+  }
+})
+  .animation('.movie-details', function() {
+    return {
+      enter: function(element, done) {
+        TweenMax.fromTo( element, 1, { x: -500, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, delay: .5 });
+      }
+    }
+  });
