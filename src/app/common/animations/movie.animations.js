04c986aeb1e373ee090a15ab3ff4956d6ed5461e
Jonathan Garvey <jon.d.garvey@gmail.com>
2015-10-26 15:52:30 -0700

    javascript-animations

--- src/app/common/animations/movie.animations.js
+++ src/app/common/animations/movie.animations.js
@@ -1,1 +1,17 @@
-angular.module('Movie.animations', ['ngAnimate']);
+angular.module('Movie.animations', ['ngAnimate'])
+    .animation('.main-content', function () {
+        return {
+            enter: function (element, done) {
+                element.hide();
+
+                element.fadeIn(function () {
+                    done();
+                });
+            },
+            leave: function (element, done) {
+                element.fadeOut(function () {
+                    done();
+                });
+            }
+        }
+    });
