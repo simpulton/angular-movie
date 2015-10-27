4b08771d88dfa53e7f31f5f13fea9c1f39e14996
Jonathan Garvey <jon.d.garvey@gmail.com>
2015-10-26 18:28:12 -0700

    greensock

--- src/app/common/animations/movie.animations.js
+++ src/app/common/animations/movie.animations.js
@@ -2,16 +2,12 @@ angular.module('Movie.animations', ['ngAnimate'])
     .animation('.main-content', function () {
         return {
             enter: function (element, done) {
-                element.hide();
-
-                element.fadeIn(function () {
-                    done();
-                });
+                TweenMax.fromTo( element, 1, { x:  -element.width(), y: 0, autoAlpha: 0 },
+                    { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, onComplete: done});
             },
             leave: function (element, done) {
-                element.fadeOut(function () {
-                    done();
-                });
+                TweenMax.fromTo( element, 1, { x: 0, y: 0, autoAlpha: 1 },
+                    { x: element.width(), y: 0, autoAlpha: 0, ease: Expo.easeInOut, onComplete: done});
             }
         }
     });
