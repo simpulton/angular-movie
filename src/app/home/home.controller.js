e47eefa1f22ae604eb9ac42dbb26676712503ee3
Jonathan Garvey <jon.d.garvey@gmail.com>
2015-10-27 14:07:09 -0700

    04-angular-templates

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -2,5 +2,8 @@ angular.module('Movie.home')
     .controller('HomeController', function () {
         var homeVm = this;
 
+        homeVm.title = "angular";
+        homeVm.release = "09.04.2015";
+
         console.log('HomeController');
     });
