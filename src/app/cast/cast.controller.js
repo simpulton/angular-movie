4036656a79ef6662671548f3cd79bca3100264dc
Jonathan Garvey <jon.d.garvey@gmail.com>
2015-10-28 13:37:00 -0700

    10-cast-page

--- src/app/cast/cast.controller.js
+++ src/app/cast/cast.controller.js
@@ -1,6 +1,11 @@
 angular.module('Movie.cast')
-    .controller('CastController', function (movie) {
+    .controller('CastController', function (movie, $sce) {
         var castVm = this;
 
         castVm.movie = movie;
+        castVm.currentCast = 0;
+
+        castVm.renderHtml = function (html) {
+            return $sce.trustAsHtml(html);
+        };
     });
