3dda373bdaf60f49a0f30442efb9ff7d87b221a6
Jonathan Garvey <jon.d.garvey@gmail.com>
2015-10-29 12:09:48 -0700

    14-javascript-animations

--- src/app/common/animations/movie.animations.js
+++ src/app/common/animations/movie.animations.js
@@ -1,1 +1,13 @@
-angular.module('Movie.animations', ['ngAnimate']);
+angular.module('Movie.animations', ['ngAnimate'])
+    .animation('.main-content', function () {
+        return {
+            enter: function (element, done) {
+                console.log('page animation in');
+                done();
+            },
+            leave: function (element, done) {
+                console.log('page animation out');
+                done();
+            }
+        }
+    })
