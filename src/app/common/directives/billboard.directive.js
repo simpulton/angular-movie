945496cb539b047adb1198902302328f7e1949a1
Jonathan Garvey <jon.d.garvey@gmail.com>
2015-10-29 14:08:54 -0700

    Add billboard animation

--- src/app/common/directives/billboard.directive.js
+++ src/app/common/directives/billboard.directive.js
@@ -1,13 +1,19 @@
 angular.module('Movie.directives.billboard', [])
 .directive('billboard', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'app/common/templates/billboard.tpl.html',
-    controller: function () {},
-    controllerAs: 'billboardVm',
-    scope: true,
-    bindToController: {
-      movie: '='
+    function link(scope, element, attrs) {
+        TweenMax.fromTo( element, 1, { x: -500, y: 0, autoAlpha: 0 }, { x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut, delay: 0.5 });
     }
-  }
+
+    return {
+        restrict: 'E',
+        link: link,
+        templateUrl: 'app/common/templates/billboard.tpl.html',
+        controller: function () {
+        },
+        controllerAs: 'billboardVm',
+        scope: true,
+        bindToController: {
+            movie: '='
+        }
+    }
 });
