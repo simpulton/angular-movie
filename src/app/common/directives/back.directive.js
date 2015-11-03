aea3d3aa378010cf7bbb08c754c1a057bd7d9f9b
Jonathan Garvey <jon.d.garvey@gmail.com>
2015-11-03 14:46:26 -0700

    Turn back button into a directive

---
+++ src/app/common/directives/back.directive.js
@@ -0,0 +1,7 @@
+angular.module('Movie.directives.back', [])
+    .directive('back', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/common/templates/back-button.tpl.html'
+        }
+    });
