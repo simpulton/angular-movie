7428cd42e89e9081067199d6651e78c25f261ea0
Jonathan Garvey <jon.d.garvey@gmail.com>
2015-11-03 15:22:44 -0700

    Remove startIndex filter

--- src/app/common/filters/start-index.filter.js
+++
@@ -1,6 +0,0 @@
-angular.module('Movie.filters.startIndex', [])
-    .filter('startIndex', function () {
-        return function (input, startIndex) {
-            return input.slice(startIndex, input.length);
-        }
-    });
