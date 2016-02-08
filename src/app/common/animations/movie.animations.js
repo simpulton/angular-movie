2ccd18dc7e7beaf78bdf18be7dec1357bc25c174
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-02-08 12:35:05 -0700

    cleanup

--- src/app/common/animations/movie.animations.js
+++ src/app/common/animations/movie.animations.js
@@ -1,13 +1,14 @@
 angular.module('Movie.animations', ['ngAnimate'])
-    .animation('.main-content', function () {
-        return {
-            enter: function (element, done) {
-                console.log('page animation in');
-                done();
-            },
-            leave: function (element, done) {
-                console.log('page animation out');
-                done();
-            }
-        }
-    });
+
+.animation('.main-content', function() {
+  return {
+    enter: function(element, done) {
+      console.log('page animation in');
+      done();
+    },
+    leave: function(element, done) {
+      console.log('page animation out');
+      done();
+    }
+  }
+});
