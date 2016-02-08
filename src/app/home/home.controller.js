a155c6b2b0da778aaf3f5e22ac363b31213241c2
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-02-08 12:35:05 -0700

    cleanup

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -1,7 +1,8 @@
 angular.module('Movie.home')
-    .controller('HomeController', function () {
-        var homeVm = this;
 
-        homeVm.title = "angular";
-        homeVm.release = "09.04.2015";
-    });
+.controller('HomeController', function() {
+  var homeVm = this;
+
+  homeVm.title = "angular";
+  homeVm.release = "09.04.2015";
+});
