ead3e27b413e815568b34c083d931317b3bc426e
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-02-08 12:35:05 -0700

    cleanup

--- src/app/synopsis/synopsis.js
+++ src/app/synopsis/synopsis.js
@@ -1,15 +1,15 @@
 angular.module('Movie.synopsis', [])
-    .config(function ($stateProvider) {
-        $stateProvider
-            .state('Movie.synopsis', {
-                url: '/synopsis',
-                views: {
-                    'main@': {
-                        templateUrl: 'app/synopsis/synopsis.tpl.html',
-                        controller: 'SynopsisController',
-                        controllerAs: 'synopsisVm'
-                    }
-                }
-            }
-        );
+
+.config(function($stateProvider) {
+  $stateProvider
+    .state('Movie.synopsis', {
+      url: '/synopsis',
+      views: {
+        'main@': {
+          templateUrl: 'app/synopsis/synopsis.tpl.html',
+          controller: 'SynopsisController',
+          controllerAs: 'synopsisVm'
+        }
+      }
     });
+});
