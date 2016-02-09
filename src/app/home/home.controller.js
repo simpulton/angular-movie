aa95f37553315f9f808a9a1c579677eaa667a014
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-02-09 13:45:16 -0700

    update(*.html, HomeController): release date

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -4,6 +4,6 @@ angular.module('Movie.home')
   var homeVm = this;
 
   homeVm.title = 'angular';
-  homeVm.release = '09.04.2015';
+  homeVm.release = '05.04.2016';
   homeVm.byline = 'SEE IT IN realD 3D, HFR 3D AND IMAX 3D'
 });
