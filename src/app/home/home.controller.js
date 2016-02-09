a15e3230d1e13c4035a67ad95aa076ceee1bcbb5
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-02-09 13:39:05 -0700

    update(HomeController): bind byline to controller and view

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -3,6 +3,7 @@ angular.module('Movie.home')
 .controller('HomeController', function() {
   var homeVm = this;
 
-  homeVm.title = "angular";
-  homeVm.release = "09.04.2015";
+  homeVm.title = 'angular';
+  homeVm.release = '09.04.2015';
+  homeVm.byline = 'SEE IT IN realD 3D, HFR 3D AND IMAX 3D'
 });
