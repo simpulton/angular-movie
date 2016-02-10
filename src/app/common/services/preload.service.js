244a772023f5b2f0ad4418ebb68c292aa3e449ad
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-02-10 15:34:27 -0700

    remove(PreloadService): redundant code

--- src/app/common/services/preload.service.js
+++ src/app/common/services/preload.service.js
@@ -3,25 +3,17 @@ angular.module('Movie.services.preload', [])
 .service('PreloadService', function ($rootScope, $q) {
   var queue = new createjs.LoadQueue(true);
 
-  var loadBgs = function (manifest) {
-    var bgs = [
+  var loadBgs = function () {
+    return [
       { src: "assets/images/background.jpg" },
       { src: "assets/images/bg-synopsis.jpg" },
       { src: "assets/images/bg-cast.jpg "}
     ];
-
-    angular.forEach(bgs, function (bg) {
-      manifest.push(bg);
-    });
-
-    return manifest;
   };
 
   this.loadManifest = function (movie) {
-    var deferred = $q.defer();
-    var manifest = [];
-
-    manifest = loadBgs(manifest);
+    var deferred = $q.defer(),
+        manifest = loadBgs();
 
     movie.images.filter(function (image) {
       manifest.push(image);
@@ -30,7 +22,6 @@ angular.module('Movie.services.preload', [])
     if (manifest.length == 0) deferred.resolve();
 
     queue.on('complete', function () {
-      $rootScope.$broadcast('queueComplete', manifest);
       deferred.resolve();
     });
 
