360851ad29666806a3c94c79b255d6f7f3d0ac23
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-02-10 15:34:27 -0700

    refactor(PreloadService): clean up code

--- src/app/common/services/preload.service.js
+++ src/app/common/services/preload.service.js
@@ -1,40 +1,32 @@
 angular.module('Movie.services.preload', [])
-    .service('PreloadService', function ($rootScope, $q) {
-        var queue = new createjs.LoadQueue(true);
 
-        var loadBgs = function (manifest) {
-            var bgs = [
-                {src: "assets/images/background.jpg"},
-                {src: "assets/images/bg-synopsis.jpg"},
-                {src: "assets/images/bg-cast.jpg"}
-            ];
+.service('PreloadService', function ($rootScope, $q) {
+  var queue = new createjs.LoadQueue(true);
 
-            angular.forEach(bgs, function (bg) {
-                manifest.push(bg);
-            });
+  var loadBgs = function () {
+    return [
+      { src: "assets/images/background.jpg" },
+      { src: "assets/images/bg-synopsis.jpg" },
+      { src: "assets/images/bg-cast.jpg "}
+    ];
+  };
 
-            return manifest;
-        };
+  this.loadManifest = function (movie) {
+    var deferred = $q.defer(),
+        manifest = loadBgs();
 
-        this.loadManifest = function (movie) {
-            var deferred = $q.defer();
-            var manifest = [];
-
-            manifest = loadBgs(manifest);
-
-            movie.images.filter(function (image) {
-                manifest.push(image);
-            });
-
-            if (manifest.length == 0) deferred.resolve();
-
-            queue.on('complete', function () {
-                $rootScope.$broadcast('queueComplete', manifest);
-                deferred.resolve();
-            });
-
-            queue.loadManifest(manifest);
-
-            return deferred.promise;
-        };
+    movie.images.filter(function (image) {
+      manifest.push(image);
     });
+
+    if (manifest.length == 0) deferred.resolve();
+
+    queue.on('complete', function () {
+        deferred.resolve();
+    });
+
+    queue.loadManifest(manifest);
+
+    return deferred.promise;
+  };
+});
